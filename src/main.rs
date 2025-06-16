1cc0269d27cdece65b59301523727c2fa0b5c18c
HAMEL <75175897+MatthieuHAMEL@users.noreply.github.com>
2025-06-16 23:35:07 +0200

    Fixed big reasoning mistake

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ const GRID_WIDTH: usize = 10;
 
 #[derive(Resource)]
 struct Grid {
-  grid: [[Option<Cell>; GRID_WIDTH]; GRID_HEIGHT],
+  grid: [[bool; GRID_WIDTH]; GRID_HEIGHT],
   // Dimensions in world unit:
   block_sz: f32,
   width_sz: f32,
@@ -27,7 +27,7 @@ struct Grid {
 impl Grid {
   pub fn new() -> Self {
     const DEFAULT_BLOCK_SZ : f32 = 26.0;
-    let mut grid = [[None; GRID_WIDTH]; GRID_HEIGHT];
+    let grid = [[false; GRID_WIDTH]; GRID_HEIGHT];
     Self {
       grid,
       block_sz: DEFAULT_BLOCK_SZ,
@@ -43,20 +43,21 @@ struct GridZoneMarker;
 #[derive(Component)]
 struct ActiveBlock; // To mark sprites that are subject to gravity and should be destroyed every second
 
+
 #[derive(Component)]
-struct PassiveBlock {x: usize, y: usize}
+struct GridPosition {
+  col: usize,
+  row: usize
+}
+
+// Each block is a sprite + a GridPosition + a marker ActiveBlock
+// They do not need to know their parent.
+
+#[derive(Component)]
+struct PassiveBlock;
 
 #[derive(Event)]
 struct NeedTetroEvent;
-
-/* This event is triggered when the grid (the passive blocks) changes, i.e. :
-- When a tetro is locked after hitting the ground
-- When a line is cleared
-When it is triggered, we want to update the sprite positions or clear the sprites that disappeared.
-For now it is a unit struct but it could contain values like the number of the line that disappeared
-*/
-#[derive(Event)]
-struct GridChangedEvent;
 
 #[derive(Resource)]
 struct GravityTimer(Timer);
@@ -101,7 +102,6 @@ fn spawn_grid_zone(
     },
     Transform::from_xyz(x, 0.0, -1.0), // behind the blocks
     GridZoneMarker
-    //Name::new("GridRoot"),
   ));
 }
 
@@ -110,56 +110,49 @@ fn block_transform(col: usize, row: usize, grid: &Grid) -> Transform {
   let x = col as f32 * grid.block_sz + grid.block_sz / 2.0 - grid.width_sz / 2.0;
   let y = -(row as f32 * grid.block_sz + grid.block_sz / 2.0) + grid.height_sz / 2.0;
 
+  debug!("block_transform -> abs={},ord={}", x, y);
   Transform::from_xyz(x, y, 0.0)
 }
 
-fn spawn_passive_block(mut commands: Commands, col: usize, row: usize, color: Color, grid: &Grid, grid_zone: Entity) {
-  commands.spawn((
-    Sprite {
-      color,
-      custom_size: Some(Vec2::splat(grid.block_sz)),
-      ..Default::default()
-    },
-    block_transform(col, row, &grid),
-    ChildOf(grid_zone),
-  ));
-}
-
-// Draws the grid and the passive blocks
-fn draw_grid(
-  mut commands: Commands,
-  grid_zone: Single<Entity, With<GridZoneMarker>>, // The entity returned by spawn_grid_root
-  grid: Res<Grid>,
-){
-  for row in 0..GRID_HEIGHT {
-    for col in 0..GRID_WIDTH {
-      if let Some(cell) = &grid.grid[row][col] {
-        // Local position relative to grid root, origin is top-left
-        commands.spawn((
-          Sprite {
-            color: cell.color,
-            custom_size: Some(Vec2::splat(grid.block_sz)),
-            ..Default::default()
-          },
-          block_transform(col, row, &grid),
-//	  ActiveBlock,
-          ChildOf(*grid_zone),
-        ));
-      }
-    }
-  }
-}
-
 // On NeedTetroEvent
-fn spawn_tetro(trigger: Trigger<NeedTetroEvent>, mut commands: Commands, grid: Res<Grid>) {
+fn spawn_tetro(
+  _trigger: Trigger<NeedTetroEvent>,
+  mut commands: Commands,
+  grid_zone: Single<Entity, With<GridZoneMarker>>,
+  grid: Res<Grid>) // TODO NO
+{
   // Get the middle coordinate of the grid
   let middle: usize = GRID_WIDTH / 2; // TODO wrong 
   debug!("Spawning tetro at column {}", middle);
 
+  let coos = [(middle-1, 0), (middle, 0), (middle+1, 0), (middle, 1)];
+
+  let tetro_id = commands.spawn((
+    Tetro { coos, kind: TetroKind::T, config: TetroRot::Base },
+    ChildOf(*grid_zone),
+    Transform::default(),
+    Visibility::default()
+  )).id();  
+  
+  // Spawn the corresponding sprites as children of the Tetro
+  for &(col, row) in coos.iter() {
+    commands.spawn((
+      Sprite {
+        color: LinearRgba::GREEN.into(),
+        custom_size: Some(Vec2::splat(grid.block_sz)), // TODO, block_sz doesn't have anything to do in grid ! 
+        ..Default::default()
+      },
+      block_transform(col, row, &grid), // IDEM !!! 
+      ActiveBlock,
+      GridPosition {col, row},
+      ChildOf(tetro_id),
+    ));
+  }
   // For now just I spawn a T block
   // TODO create a helper function with default pos for all types of blocks
   // TODO 2 : here I could check that my new tetro's coordinates don't overlap with the grid
-  commands.spawn(Tetro { coos: [(middle-1, 0), (middle, 0), (middle+1, 0), (middle, 1)], kind: TetroKind::T, config: TetroRot::Base });
+
+  
 }
 
 // HELPER
@@ -168,14 +161,18 @@ fn tetro_can_move_down(tetro: &Tetro, grid: &Grid) -> bool {
     if coord.1 == GRID_HEIGHT - 1 { // floor -> can't move down!
       return false;
     }
-    grid.grid[coord.1 + 1][coord.0].is_none()
+    debug!("looking at grid[{}][{}]", coord.1 + 1, coord.0);
+    !grid.grid[coord.1 + 1][coord.0]
   })
 }
 
 // Grid is filled only when we LOCK the tetromino
 fn apply_gravity(
-  mut commands: Commands, mut tetros: Query<(Entity, &mut Tetro)>,
+  mut commands: Commands,
+  mut tetros: Query<(Entity, &mut Tetro, &Children)>,
+  mut blocks: Query<(&mut GridPosition, &mut Transform), With<ActiveBlock>>,
   mut grid: ResMut<Grid>,
+  grid_zone: Single<Entity, With<GridZoneMarker>>,
   mut gravity_timer: ResMut<GravityTimer>, time: Res<Time>)
 {
   gravity_timer.0.tick(time.delta());
@@ -183,18 +180,26 @@ fn apply_gravity(
     return;
   }
   
-  for (entity, mut tetro) in tetros.iter_mut() {
+  for (entity, mut tetro, children) in tetros.iter_mut() {
     if tetro_can_move_down(&tetro, &grid) {
-      // move down
+      // Move down
       for coord in tetro.coos.iter_mut() {
         coord.1 += 1;
       }
+
+      // Update the children blocks GridPosition and Transform
+      for (child, &(col, row)) in children.iter().zip(tetro.coos.iter()) {
+        if let Ok((mut grid_pos, mut transform)) = blocks.get_mut(child) {
+          grid_pos.col = col;
+          grid_pos.row = row;
+          *transform = block_transform(col, row, &grid); // update visual pos
+        }
+      }
     } else {
       // lock in grid and despawn tetro
-      for coord in tetro.coos.iter() {
-        grid.grid[coord.1][coord.0] = Some(Cell { color: LinearRgba::BLUE.into(), sprite: None });
-      }
-      // Despawn the tetro
+      lock_tetro(&mut commands, &children, &mut blocks, &mut grid, &grid_zone);
+      
+      // Despawn the tetro. Sprites / blocks have been reparented in lock_tetro.
       commands.entity(entity).despawn();
       // Emit event to spawn a new tetro
       commands.trigger(NeedTetroEvent);
@@ -203,32 +208,25 @@ fn apply_gravity(
   }
 }
 
-fn clear_moving_blocks(mut commands: Commands, blocks: Query<Entity, With<ActiveBlock>>) {
-  for entity in blocks.iter() {
-    commands.entity(entity).despawn();
-  }
-}
-
-fn draw_tetros(
-  mut commands: Commands,
-  grid_zone: Single<Entity, With<GridZoneMarker>>,
-  tetros: Query<&Tetro>,
-  grid: Res<Grid>)
+fn lock_tetro(
+  commands: &mut Commands,
+  children: &Children,
+  block_query: &mut Query<(&mut GridPosition, &mut Transform), With<ActiveBlock>>,
+  grid: &mut Grid,
+  grid_zone: &Single<Entity, With<GridZoneMarker>>)
 {
-  for tetro in tetros.iter() {
-     for (col, row) in tetro.coos.iter() {
-       commands.spawn((
-         Sprite {
-           color: LinearRgba::GREEN.into(), // TODO meaningful color
-           custom_size: Some(Vec2::splat(grid.block_sz)),
-           ..Default::default()
-         },
-         block_transform(*col, *row, &grid),
-	 ActiveBlock, 
-         ChildOf(*grid_zone),
-       ));
-     }
-  }
+    for block_entity in children.iter() {
+      if let Ok((grid_pos, _)) = block_query.get_mut(block_entity) {
+        // Update the grid data
+        grid.grid[grid_pos.row][grid_pos.col] = true;
+        // Remove ActiveBlock, add PassiveBlock
+        commands.entity(block_entity)
+          .remove::<ActiveBlock>()
+          .insert(PassiveBlock); // Maybe not needed ? TODO 
+        // Detach from tetromino parent (or re-parent to the grid root)
+        commands.entity(block_entity).remove::<ChildOf>().insert(ChildOf(**grid_zone));
+      }
+    }
 }
 
 fn send_initial_tetro_event(mut commands: Commands) {
@@ -255,13 +253,7 @@ fn main() {
   );
 
   // Systems that run once per frame
-  app.add_systems(
-    Update, 
-    (draw_grid , /* systems with .after, .before, .run_if ...*/
-     draw_tetros,
-     clear_moving_blocks.before(draw_tetros),
-     apply_gravity) 
-  );
+  app.add_systems(Update, apply_gravity); // with a timer 
   
   // TODO spawn_grid_background also when window is resized
   // RESOURCES
